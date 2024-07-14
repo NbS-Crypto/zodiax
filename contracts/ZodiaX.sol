ba2cf9a18d3e1353e90543e5ae0712330c8ca30c
Philipp <neurohrph.git.neurohr@bytes.software>
2024-07-14 17:31:57 +0200

    refactor: explicit imports

--- contracts/ZodiaX.sol
+++ contracts/ZodiaX.sol
@@ -2,9 +2,9 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /// @custom:security-contact matrix-plant-pulse@duck.com
 contract ZodiaX is ERC20, ERC20Burnable, ERC20Permit {
